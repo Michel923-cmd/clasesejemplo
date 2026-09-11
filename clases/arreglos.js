d49e6d116237bf5a547f531bb986472b2badda37
Michel923-cmd <mchantreramirez@gmail.com>
2026-09-11 11:00:55 -0500

    quinto cambio

---
+++ clases/arreglos.js
@@ -0,0 +1,15 @@
+//push=>agregar al final
+let lista=[]
+lista.push("a")
+console.log(lista)
+lista.push("b")
+console.log(lista)
+//unshift=>agregar al inicio de un arreglo
+lista.unshift("c")
+console.log(lista)
+//pop=>eliminar elemto al final
+lista.pop()
+console.log(lista)
+//shift=>elimina al inicio de la lista
+lista.shift()
+console.log(lista)
--- clases/index.js
+++ clases/index.js
@@ -1,3 +1,65 @@
-console.log("holaaaaaaaaa")
-console.log("soy Michel")
-console.log("tengo 17 años")
+//una veterinaria necesita desarrolar un sistema para la atencion d mascotas
+//animal, propietario, atencion 
+
+class Animal{
+    nombre
+    raza
+    especie
+    edad
+    constructor (nombre,raza,especie,edad){
+        this.nombre=nombre
+        this.raza=raza
+        this.especie=especie
+        this.edad=edad
+    }
+    registrarNombre(nuevoNombre){
+        this.nombre=nuevoNombre
+    }
+    
+    registrarRaza(nuevaRaza){
+        this.raza=nuevaRaza
+    }
+    registrarEspecie(nuevaEspecie){
+        this.especie=nuevaEspecie
+    }
+    registrarEdad(nuevaEdad){
+        this.edad=nuevaEdad
+    }
+    mostrarInformacion(){
+        console.log(this.nombre+" "+this.raza)
+    }
+}
+
+class Propietario{
+    nombrePropietario
+    Direccion
+    telefono
+    constructor(nombrePropietario,direccion,telefono){
+    this.nombrePropietario=nombrePropietario
+    this.direccion=direccion
+    this.telefono=telefono
+    this.mascota=[]
+    }
+    registrarNombre(nuevoNombre){
+        this.nombrePropietario=nuevoNombre
+    }
+    registrarDireccion(nuevaDireccion){
+        this.direccion=nuevaDireccion
+    }
+    registarTelefono(nuevoTelefono){
+        this.telefono=nuevoTelefono
+    }
+    registrarMascota(nuevamascota){
+        //pop,unshift,shift,push
+        this.mascota.push(nuevamascota)
+    }
+    mostrarInformacion(){
+        console.log("Mascota:"+this.mascota[0].nombre+ " Propietario:"+this.nombrePropietario)
+    }
+}
+
+let colmillo=new Animal("colmillo","pitbull","perro",12)
+colmillo.mostrarInformacion()
+let alexa=new Propietario("Alexa","123","1111")
+alexa.registrarMascota(colmillo)
+alexa.mostrarInformacion()
